e2706243612a5068ea13e19d001eb17cd390ec6c
Peterisabu17 <142930056+Peterisabu17@users.noreply.github.com>
2024-01-25 04:04:09 +0100

    fix

--- swiper-card.js
+++ swiper-card.js
@@ -7,9 +7,9 @@ var swiper = new Swiper(".slide-content", {
     enabled: true,
   },
   breakpoints: {
-    450: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+    326: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
     },
     769: {
       slidesPerView: 2,
